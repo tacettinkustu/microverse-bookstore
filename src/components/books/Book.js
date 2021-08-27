38fcd3488f42596ab3363248c363a206a13ba277
Tacettin Kustu <tacettinkustu@gmail.com>
2021-08-27 23:54:27 +0300

    Add functionality

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,17 +1,10 @@
 import PropTypes from 'prop-types';
 import Progress from './BookProgress';
-
-const Actions = () => (
-  <div>
-    <button type="button">Comments</button>
-    <button type="button">Remove</button>
-    <button type="button">Edit</button>
-  </div>
-);
+import ActionButtons from './ActionButtons';
 
 const Book = ({ book }) => {
   const {
-    title, category, author, progress,
+    id, title, category, author, progress,
   } = book;
 
   return (
@@ -24,18 +17,18 @@ const Book = ({ book }) => {
         </div>
         <Progress progress={progress} />
       </div>
-      <Actions />
+      <ActionButtons id={id} />
     </div>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
     progress: Progress.propTypes.progress,
   }).isRequired,
 };
-
 export default Book;
