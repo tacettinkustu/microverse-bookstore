4ecc00c7d17c603429379e4cbcc4edd3c980b9ec
Tacettin Kustu <tacettinkustu@gmail.com>
2021-08-27 19:34:44 +0300

    Leverage app and nav components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-const App = () => <h1>Bookstore</h1>;
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Books from './components/books/Books';
+import Categories from './components/categories/Categories';
+import Nav from './components/Nav';
+
+const routes = [
+  {
+    path: '/',
+    name: 'BOOKS',
+    component: <Books />,
+  },
+  {
+    path: '/categories',
+    name: 'CATEGORIES',
+    component: <Categories />,
+  },
+];
+
+const App = () => (
+  <Router>
+    <Nav title="Bookstore CMS" routes={routes} />
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact key={path}>
+          {component}
+        </Route>
+      ))}
+    </Switch>
+  </Router>
+);
 
 export default App;
