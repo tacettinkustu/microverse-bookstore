5d610833f05d5a0098a1a49f2f0d19f58e27c44f
Tacettin Kustu <tacettinkustu@gmail.com>
2021-08-28 15:57:25 +0300

    Get books to the page

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 import * as API from '../../api';
 
+const LOAD_BOOKS = 'bookStore/books/LOAD_BOOKS';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
@@ -7,28 +8,37 @@ const initialState = [];
 
 API.createApp();
 
+const loadBooks = (payload) => ({
+  type: LOAD_BOOKS,
+  payload,
+});
+
 const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
-
 const removeBook = (id) => ({
   type: REMOVE_BOOK,
   id,
 });
-
 export const createBook = (book) => async (dispatch) => {
   const isCreated = await API.createBook(book);
   if (isCreated) {
     dispatch(addBook(book));
   }
 };
-
 export const deleteBook = (id) => async (dispatch) => {
   const isDeleted = await API.deleteBook(id);
-
   if (isDeleted) {
     dispatch(removeBook(id));
+  }
+};
+
+export const getBooks = () => async (dispatch) => {
+  const books = await API.getBooks();
+
+  if (books) {
+    dispatch(loadBooks(books));
   }
 };
 
@@ -38,6 +48,8 @@ const reducer = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
+    case LOAD_BOOKS:
+      return Object.values(action.payload).map(([book]) => book);
     default:
       return state;
   }
