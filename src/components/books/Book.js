4666c86e4958c695053913726ea159e5ad3bae89
Tacettin Kustu <tacettinkustu@gmail.com>
2021-08-28 16:49:09 +0300

    Make modifications

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,34 +1,31 @@
 import PropTypes from 'prop-types';
 import Progress from './BookProgress';
 import ActionButtons from './ActionButtons';
+import './Books.css';
 
 const Book = ({ book }) => {
-  const {
-    item_id: id, title, category, author, progress,
-  } = book;
+  const { item_id: id, title, category, author, progress } = book;
 
   return (
-    <div>
-      <div>
-        <div>
-          <h4>{category}</h4>
-          <h2>{title}</h2>
-          <h6>{author}</h6>
+    <div className="book">
+      <div className="book-content">
+        <div className="book-info">
+          <h4 className="book-category">{category}</h4>
+          <h2 className="book-title">{title}</h2>
+          <h6 className="book-author">{author}</h6>
+          <ActionButtons id={id} />
         </div>
         <Progress progress={progress} />
       </div>
-      <ActionButtons id={id} />
     </div>
   );
 };
-
 Book.propTypes = {
   book: PropTypes.shape({
     item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
-
     progress: Progress.propTypes.progress,
   }).isRequired,
 };
