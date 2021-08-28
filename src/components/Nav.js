e7adfa6f00287b902ea0354a0884c6115a631aa8
Tacettin Kustu <tacettinkustu@gmail.com>
2021-08-28 16:40:39 +0300

    Leverage nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
+import './Nav.css';
 
 const Nav = ({ title, routes }) => (
-  <nav>
+  <nav className="nav-bar">
     <Link to="/">{title}</Link>
-    <ul>
+    <ul className="nav-links">
       {routes.map(({ name, path }) => (
         <li key={path}>
           <NavLink exact to={path}>
@@ -15,7 +16,6 @@ const Nav = ({ title, routes }) => (
     </ul>
   </nav>
 );
-
 Nav.propTypes = {
   title: PropTypes.string.isRequired,
   routes: PropTypes.arrayOf(
